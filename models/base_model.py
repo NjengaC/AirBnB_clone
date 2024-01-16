e4f68b4af6aff5cd8069bb1ac0c4e1a18cc3bd6b
NjengaC <victorcyrus01@gmail.com>
2024-01-17 02:01:51 +0300

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -16,20 +16,25 @@ class BaseModel:
         """
         Constructor for BaseModel
         """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
-        else:
+        if kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key == 'id':
-                    setattr(self, key, value)
-                elif key == 'created_at' or key == 'updated_at':
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.fromisoformat(value))
-                elif key == '__class__':
-                    pass
                 else:
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -43,14 +48,17 @@ class BaseModel:
          update the pub instance attr updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
         Returns a dict containing all keys/values of __dict__ of the instance.
         """
         instance_dict = self.__dict__.copy()
-        instance_dict['created_at'] = self.created_at.isoformat()
-        instance_dict['updated_at'] = self.updated_at.isoformat()
-        instance_dict['__class__'] = self.__class__.__name__
+
+        instance_dict.update({'__class__': self.__class__.__name__,
+                              'created_at': self.created_at.isoformat(),
+                              'updated_at': self.updated_at.isoformat(),
+                              })
         return instance_dict
