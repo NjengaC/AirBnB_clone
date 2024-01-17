3bd95e83952f41bfb55654bdca6cb3d2e215653a
NjengaC <victorcyrus01@gmail.com>
2024-01-17 03:45:04 +0300

    city state amenity place

--- models/state.py
+++ models/state.py
@@ -1,48 +1,9 @@
-# models/state.py
+#!/usr/bin/python3
+"""A class state"""
+import models
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """State class that inherits from BaseModel."""
+    """Defines the class State"""
     name = ""
-
-# models/city.py
-
-
-class City(BaseModel):
-    """City class that inherits from BaseModel."""
-    state_id = ""
-    name = ""
-
-# models/amenity.py
-
-
-class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel."""
-    name = ""
-
-# models/place.py
-
-
-class Place(BaseModel):
-    """Place class that inherits from BaseModel."""
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
-
-# models/review.py
-
-
-class Review(BaseModel):
-    """Review class that inherits from BaseModel."""
-    place_id = ""
-    user_id = ""
-    text = ""
--- models/user.py
+++ models/user.py
@@ -14,8 +14,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-
-def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    storage.new(self)
